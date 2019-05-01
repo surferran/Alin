fa391d9d695a0aa7ecd3325129eaff35ab824b68
ran <sern@yahoo.com>
2019-05-01 07:45:35 +0300

    1st code to learn operation

---
+++ code/HelloServer_trial/HelloServer_trial.ino
@@ -0,0 +1,95 @@
+/**
+ * source from Examples
+ * 
+ * after 'burn' try in http:
+ *  http://10.0.0.70/
+ *  http://10.0.0.70/?hhvjv 
+ *  http://10.0.0.70/arg?arg1=5;analog1=3
+ *  http://10.0.0.70/inline
+ *  
+ *  why must it connect to local router to act as wifi server??
+ */
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+#include "private_server_details.h"
+
+#ifndef STASSID
+#define STASSID "your-ssid"
+#define STAPSK  "your-password"
+#endif
+
+const char* ssid = STASSID;
+const char* password = STAPSK;
+
+ESP8266WebServer server(80);
+
+const int led = LED_BUILTIN; //13;
+
+void handleRoot() {
+  digitalWrite(led, 1);
+  server.send(200, "text/plain", "hello from esp8266!.  \n saw your led blinks?? ");
+  delay(500);
+  digitalWrite(led, 0);
+}
+
+void handleNotFound() {
+  digitalWrite(led, 1);
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+  digitalWrite(led, 0);
+}
+
+void setup(void) {
+  pinMode(led, OUTPUT);
+  digitalWrite(led, 0);
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("esp8266")) {
+    Serial.println("MDNS responder started");
+  }
+
+  server.on("/", handleRoot);
+
+  server.on("/inline", 
+    []() {
+      server.send(200, "text/plain", "this works as well");
+    }
+    );
+
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop(void) {
+  server.handleClient();
+  MDNS.update();
+}
---
+++ code/sketch_apr26_esp8266mod_start.ino
@@ -0,0 +1,121 @@
+/**
+ * starting project with esp8266mod hardware:
+ * go by :
+ * https://dzone.com/articles/programming-the-esp8266-with-the-arduino-ide-in-3
+ * which says:
+ * -download Arduino IDE
+ * -set some preferences
+ *  (and on the it refers to http://arduino.esp8266.com/stable/package_esp8266com_index.json)
+ * - then find example in  
+ *   https://help.ubidots.com/connect-your-devices/connect-a-nodemcu-esp8266-to-ubidots-over-http#send-one-value-to-ubidots
+ *   or others..
+ *   (ubidots is a server for IoT data.
+ *    relevenat lib can be found here:
+ *    https://github.com/ubidots/ubidots-esp8266
+ *    )
+ *    
+ *  hardware used: 
+ *  similar to 'Lolin D1 mini Lite'  https://www.instructables.com/id/Programming-the-ESP8266MOD-ESP-12-Module-Using-the/ 
+ *  (choose the above in the Arduino IDE for used hardware (in Tools->Board)
+ *  (also for saftey choose upload freq to 115200, not higher)
+ *  
+ *  possible 'literature':
+ *  - 'Wemos D1 mini: A first look at this ESP8266 ' https://www.youtube.com/watch?v=AuFXuEYd1Yk
+ *  - pins sketch https://escapequotes.net/esp8266-wemos-d1-mini-pins-and-diagram/
+ *  - https://www.electroschematics.com/12012/rotary-encoder-arduino/
+ *  
+ *  examplary serial output at 115200:
+ *  
+led: 0
+led: 1
+led: 0
+10:18:09.716 -> led: 1
+10:18:10.019 -> led: 0
+10:18:14.517 -> led: 1
+10:18:14.855 -> led: 0
+
+10:18:19.353 -> led: 1
+10:18:19.661 -> led: 0
+10:18:24.185 -> led: 1
+10:18:24.493 -> led: 0
+
+10:18:28.991 -> led: 1
+10:18:29.297 -> led: 0
+10:18:33.828 -> led: 1
+10:18:34.133 -> led: 0
+
+10:18:38.645 -> led: 1
+10:18:38.953 -> led: 0
+10:18:43.446 -> led: 1
+
+.. time is not exact..
+
+todo:
+  server operation:
+  https://www.instructables.com/id/Programming-a-HTTP-Server-on-ESP-8266-12E/
+  ide: vscode with esp8266
+  read analog (copal jc10 10k ohm). red wire of the HW potentiometer will be the Analog input.
+    yellow, and green are to the 0,5V points.
+
+ */
+
+// todo: any includes ? currently using the builtin from the IDE
+
+ /** functions headers */
+void setup();
+void loop();
+void print_led_state();
+void blink_example_cycle();
+void read_and_print_analog();
+
+#define ANALOG_PIN A0
+
+/** program content */
+void setup() 
+{
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an (digital?) output
+  pinMode(ANALOG_PIN, INPUT);
+
+  Serial.begin(115200);
+  Serial.println("");
+  Serial.println("d1 mini init is done");
+}
+
+void loop() 
+/** loop function is used for a cyclic operation, and activated over and over */
+{
+  // put your main code here, to run repeatedly:
+  blink_example_cycle();
+
+  read_and_print_analog();
+}
+
+/** utils functions */
+void print_led_state()
+{
+  int ledVal=0;
+  ledVal = digitalRead(LED_BUILTIN);
+  Serial.print("led: ");  
+  Serial.println(ledVal);
+}
+
+void blink_example_cycle() 
+{
+  // light on
+  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
+  delay(0500);                      // Wait for a 0.5 second
+  print_led_state();
+  // ligth off
+  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+  delay(4500);                      // Wait for 4.5 seconds (to demonstrate the active low LED)
+  print_led_state();
+}
+
+void read_and_print_analog()
+{
+  int analogVal=0;
+  analogVal = analogRead(ANALOG_PIN);
+  Serial.print("analogVal: ");  
+  Serial.println(analogVal);  // can do val/1023 [%] or other calculated ratio..
+}
